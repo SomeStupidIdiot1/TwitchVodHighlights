54423339f7aa9d453387c05f8c037e57347aab00
SomeStupidIdiot <somestupididiot1@gmail.com>
2020-07-14 09:24:58 -0400

    look up url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from "react";
-import { Typography, TextField, Container } from "@material-ui/core";
+import { Typography, TextField, Container, Button } from "@material-ui/core";
+import { getVodInfo } from "./twitchAPI/getVodInfo";
 
 function App() {
+  const [urls, setUrls] = React.useState("");
+  const [vodData, setVodData] = React.useState({});
+  const [errs, setErrs] = React.useState([]);
+  const lookUp = () => {
+    urls
+      .split("\n")
+      .map((url) => {
+        url = url.trim();
+        if (!isNaN(url)) return url;
+        for (let splitItem of url.split("/")) {
+          if (!isNaN(splitItem)) return splitItem;
+        }
+        setErrs(
+          errs.concat(
+            `Unable to get the url ${url}. The format is probably bad.`
+          )
+        );
+        return "";
+      })
+      .forEach((id) => {
+        if (id !== "") {
+          getVodInfo(id)
+            .then((data) => setVodData({ ...vodData, id: data }))
+            .catch(() =>
+              setErrs(
+                errs.concat(
+                  `Unable to get ${id}. Likely invalid or inaccessible.`
+                )
+              )
+            );
+        }
+      });
+      
+  };
   return (
     <Container>
-      <Typography>
+      <Typography variant="h5" component="h3">
         Put each twitch vod URL or twitch vod ID on a new line
       </Typography>
       <TextField
@@ -13,9 +48,12 @@ function App() {
         rows={4}
         variant="outlined"
         label="URLs"
-      >
-        tests
-      </TextField>
+        fullWidth
+        onChange={(e) => setUrls(e.target.value)}
+      />
+      <Button variant="contained" color="primary" onClick={lookUp}>
+        Look up URLs
+      </Button>
     </Container>
   );
 }
