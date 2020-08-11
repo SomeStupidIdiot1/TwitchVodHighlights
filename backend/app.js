b9d0f213461e095ecb0286a35e3fb636763e32f2
SomeStupidIdiot <somestupididiot1@gmail.com>
2020-08-11 03:59:01 -0400

    backend default port

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 const getVodInfo = require("./twitchAPI/getVodInfo");
 const { getQualities, getVideo } = require("./twitchAPI/getvod");
